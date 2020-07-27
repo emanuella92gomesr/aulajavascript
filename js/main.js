9fde44cf24c1adfdcaeaacec81d39270e7ca66ab
emanuella92gomesr <emanuellagomes6@gmail.com>
2020-07-26 21:53:14 -0300

    Correção

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var nome = "Emanuella Gomes";
 var idade = 27;
-var idade = 10;
+var idade2 = 10;
 var frase = "Eu quero trabalhar como programadora";
-alert(nome + "tem" + idade + "anos." = frase);
+alert(nome + "tem" + idade + "anos." + frase);
 console.log(nome);
 console.log(idade+idade2);
 console.log(frase.toLowerCase());
